4c728fcd552e1e1698a873c93dc7be7c6e6cfda9
Nifont <nifontbus@yandex.ru>
2024-03-22 12:30:49 +0300

    +reactive jwtAuthenticationConverter in other @Bean

--- server-flux/src/main/kotlin/ru/nb/serverflux/SpringSecurityConfig.kt
+++ server-flux/src/main/kotlin/ru/nb/serverflux/SpringSecurityConfig.kt
@@ -2,8 +2,10 @@ package ru.nb.serverflux
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter
@@ -15,69 +17,46 @@ class SpringSecurityConfig {
 
 	@Bean
 	fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-
-		// конвертер для настройки spring security
-//		val jwtAuthenticationConverter = JwtAuthenticationConverter()
-		// подключаем конвертер ролей
-//		jwtAuthenticationConverter.apply {
-//			setJwtGrantedAuthoritiesConverter(KCRoleConverter())
-//			setPrincipalClaimName("name")
-//		}
-
 		http
-//			.sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
 			.csrf { csrf -> csrf.disable() }
 			.authorizeExchange { auth ->
-//				auth.anyExchange().authenticated()
-//				auth.anyExchange().permitAll()
 				auth.anyExchange().hasRole("SYSTEM")
-//				auth.anyExchange().hasAnyRole("MANAGER", "ADMIN")
-//				auth.anyExchange().hasRole("manage-account")
 			}
 			.oauth2ResourceServer { config ->
-				/*	config.jwt {
-						it.jwtAuthenticationConverter(jwtAuthenticationConverter)
-					}*/
-//				config.jwt(Customizer.withDefaults())
-
-				config.jwt { jwt ->
-					val jwtAuthenticationConverter = ReactiveJwtAuthenticationConverter().apply {
-						setPrincipalClaimName("preferred_username")
-//						setPrincipalClaimName("name")
-					}
-					jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)
-
-					jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
-						ReactiveJwtGrantedAuthoritiesConverterAdapter { token ->
-							val jwtGrantedAuthoritiesConverter = JwtGrantedAuthoritiesConverter()
-							val grantedScopes = jwtGrantedAuthoritiesConverter.convert(token)
-							println("Granted scopes: $grantedScopes")
-
-							val groupsJwtGrantedAuthoritiesConverter = JwtGrantedAuthoritiesConverter().apply {
-								setAuthoritiesClaimName("groups")
-								setAuthorityPrefix("")
-							}
-							val grantedGroups = groupsJwtGrantedAuthoritiesConverter.convert(token)?.filter {
-								it.authority.startsWith("ROLE_")
-							}
-							println("Granted groups: $grantedGroups")
-
-							grantedGroups?.let {
-								grantedScopes?.addAll(it)
-							}
-
-							println("All: $grantedScopes")
-							grantedScopes
-						}
-					)
-				}
+				config.jwt(Customizer.withDefaults())
 			}
 		return http.build()
 	}
 
-//	@Bean
-//	fun grantedAuthorityDefaults(): GrantedAuthorityDefaults {
-//		return GrantedAuthorityDefaults("") // Remove the ROLE_ prefix
-//	}
+	@Bean
+	fun reactiveJwtAuthenticationConverter(): ReactiveJwtAuthenticationConverter {
+		return ReactiveJwtAuthenticationConverter().apply {
+			setPrincipalClaimName("preferred_username")
+//						setPrincipalClaimName("name")
+			setJwtGrantedAuthoritiesConverter(
+				ReactiveJwtGrantedAuthoritiesConverterAdapter { token ->
+					val jwtGrantedAuthoritiesConverter = JwtGrantedAuthoritiesConverter()
+					// Права по умолчанию
+					val grantedScopes = jwtGrantedAuthoritiesConverter.convert(token) ?: emptyList()
+					println("Granted scopes: $grantedScopes")
+
+					// Если берем роли из realm_access/roles
+					val realmAccess = token.getClaimAsMap("realm_access")
+					val roles = realmAccess["roles"] as? List<*>
+					val grantedRoles = roles?.let { roleList ->
+						roleList
+							.mapNotNull { it as? String }
+							.filter { it.startsWith("ROLE_") }
+							.map { SimpleGrantedAuthority(it) }
+					} ?: emptyList()
+
+					println("Granted roles: $grantedRoles")
+					val allGranted = grantedScopes + grantedRoles
+					println("All: $allGranted")
+					allGranted
+				}
+			)
+		}
+	}
 
 }
